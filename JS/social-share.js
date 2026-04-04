5ad2586330eeb945084c2910426a3e32557fa4ed
Somnath pal <132137996+Sparrow-gif@users.noreply.github.com>
2026-04-04 08:44:54 +0530

    Create social-share.js

---
+++ JS/social-share.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const pageUrl = window.location.href;
+  const pageTitle = document.title;
+
+  document.querySelectorAll(".share-btn").forEach(btn => {
+
+    btn.addEventListener("click", () => {
+
+      const platform = btn.getAttribute("data-platform");
+      let shareUrl = "";
+
+      switch(platform){
+
+        case "facebook":
+          shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`;
+          break;
+
+        case "twitter":
+          shareUrl = `https://twitter.com/intent/tweet?url=${pageUrl}&text=${pageTitle}`;
+          break;
+
+        case "whatsapp":
+          shareUrl = `https://api.whatsapp.com/send?text=${pageTitle} ${pageUrl}`;
+          break;
+
+        case "pinterest":
+          shareUrl = `https://pinterest.com/pin/create/button/?url=${pageUrl}`;
+          break;
+
+        case "copy":
+          navigator.clipboard.writeText(pageUrl);
+          alert("Link copied!");
+          return;
+      }
+
+      window.open(shareUrl, "_blank");
+    });
+
+  });
+
+});
