98dacd7dc53eb2707ea373d3157a8ffd65c5f19e
Somnath pal <132137996+Sparrow-gif@users.noreply.github.com>
2026-04-04 11:06:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
+const CACHE_NAME = "qmath-cache-v1";
 
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/butterfly.css",
+  "/JS/main.js",
+  "/assets/favicon.png"
+];
+
+// Install → cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch → serve from cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
